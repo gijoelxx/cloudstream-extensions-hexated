528703ede8f30c08e82b50aa2d2ef66bb77309e2
gijoelxx <technoticx@gmail.com>
2024-11-02 22:27:51 +0100

    Update Cineclix.kt

--- Moflix/src/main/kotlin/com/hexated/Cineclix.kt
+++ Moflix/src/main/kotlin/com/hexated/Cineclix.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.mainPageOf
 
 class Cineclix : Moflix() {
     override var name = "Cineclix"
-    override var mainUrl = "https://cineclix.in"
+    override var mainUrl = "https://hdfilme.my"
     override val mainPage = mainPageOf(
         "77/created_at:desc" to "Neuerscheinungen Filme",
         "82/created_at:desc" to "Neuerscheinungen Serien",
