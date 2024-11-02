95ebf53112b9fc0610af405617b83043e5badd49
gijoelxx <technoticx@gmail.com>
2024-11-03 00:28:48 +0100

    Update and rename Movie4k.kt to Cineclix.kt

---
+++ Moflix/src/main/kotlin/com/hexated/Cineclix.kt
@@ -0,0 +1,15 @@
+package com.hexated
+
+import com.lagradost.cloudstream3.mainPageOf
+
+class Cineclix : Moflix() {
+    override var name = "Movie4k"
+    override var mainUrl = "https://api.movie4k.sx"
+    
+    override val mainPage = mainPageOf(
+        "browse/?lang=2&keyword=&year=&rating=&votes=&genre=Action&country=&cast=&directors=&type=&order_by=trending&page=1&limit=100" to "Trend Filme",
+        "browse/?lang=2&keyword=&year=&rating=&votes=&genre=Drama&country=&cast=&directors=&type=&order_by=trending&page=1&limit=100" to "Drama Filme",
+        "browse/?lang=2&keyword=&year=&rating=&votes=&genre=Comedy&country=&cast=&directors=&type=&order_by=trending&page=1&limit=100" to "Komödie Filme"
+    )
+
+}
