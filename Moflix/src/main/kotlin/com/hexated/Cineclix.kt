d991700732100b2944a8e2e679f0bf5979534cc0
gijoelxx <technoticx@gmail.com>
2024-11-02 22:47:54 +0100

    Update Cineclix.kt

--- Moflix/src/main/kotlin/com/hexated/Cineclix.kt
+++ Moflix/src/main/kotlin/com/hexated/Cineclix.kt
@@ -2,13 +2,13 @@ package com.hexated
 
 import com.lagradost.cloudstream3.mainPageOf
 
-class Cineclix : Moflix() {
-    override var name = "Cineclix"
-    override var mainUrl = "https://hdfilme.my"
+class Movie4k : Moflix() {
+    override var name = "Movie4k"
+    override var mainUrl = "https://api.movie4k.sx"
+    
     override val mainPage = mainPageOf(
-        "77/created_at:desc" to "Neuerscheinungen Filme",
-        "82/created_at:desc" to "Neuerscheinungen Serien",
-        "77/popularity:desc" to "Filme",
-        "82/popularity:desc" to "Serien",
+        "browse/?lang=2&keyword=&year=&rating=&votes=&genre=Action&country=&cast=&directors=&type=&order_by=trending&page=1&limit=100" to "Trend Filme",
+        "browse/?lang=2&keyword=&year=&rating=&votes=&genre=Drama&country=&cast=&directors=&type=&order_by=trending&page=1&limit=100" to "Drama Filme",
+        "browse/?lang=2&keyword=&year=&rating=&votes=&genre=Comedy&country=&cast=&directors=&type=&order_by=trending&page=1&limit=100" to "Komödie Filme"
     )
 }
