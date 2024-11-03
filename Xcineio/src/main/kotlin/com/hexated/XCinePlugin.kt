fd458f415e182ca2ec04863ba52af0ab7338652b
gijoelxx <technoticx@gmail.com>
2024-11-03 21:46:08 +0100

    Update XCinePlugin.kt

--- Xcineio/src/main/kotlin/com/hexated/XCinePlugin.kt
+++ Xcineio/src/main/kotlin/com/hexated/XCinePlugin.kt
@@ -3,7 +3,6 @@ package com.hexated
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-
 @CloudstreamPlugin
 class FilmCloud: Plugin() {
     override fun load(context: Context) {
