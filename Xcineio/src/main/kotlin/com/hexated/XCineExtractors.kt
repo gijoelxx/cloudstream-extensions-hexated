2c4c3e50ec45ade9a75b497dcbd39ce2ea8c189f
gijoelxx <technoticx@gmail.com>
2024-11-03 23:40:27 +0100

    Update XCineExtractors.kt

--- Xcineio/src/main/kotlin/com/hexated/XCineExtractors.kt
+++ Xcineio/src/main/kotlin/com/hexated/XCineExtractors.kt
@@ -3,7 +3,6 @@ package com.hexated
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.lagradost.cloudstream3.extractors.MoflixClick
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.extractors.Filesim
