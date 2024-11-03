bb9c668eb7b1ee90a9967386978decc66ad61f84
gijoelxx <technoticx@gmail.com>
2024-11-04 00:44:40 +0100

    Update XCineExtractors.kt

--- Xcineio/src/main/kotlin/com/hexated/XCineExtractors.kt
+++ Xcineio/src/main/kotlin/com/hexated/XCineExtractors.kt
@@ -14,6 +14,14 @@ import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.getAndUnpack
 import java.net.URI
 
+// Basis-Extractor-Klasse
+open class MoflixClick : ExtractorApi() {
+    override val name = "MoflixClick"
+    override val mainUrl = "https://moflix-stream.click"
+    override val requiresReferer = true
+}
+
+// Spezifische Implementierungen der Extractor-Klassen
 class MoflixLink : MoflixClick() {
     override val name = "MoflixLink"
     override val mainUrl = "https://moflix-stream.link"
@@ -29,14 +37,7 @@ class Highstream : MoflixClick() {
     override val mainUrl = "https://highstream.tv"
 }
 
-open class MoflixClick : ExtractorApi() {
-    override val name = "MoflixClick"
-    override val mainUrl = "https://moflix-stream.click"
-    override val requiresReferer = true
-
-
-
-class VoeSx: Voe() {
+class VoeSx : Voe() {
     override val name = "Voe Sx"
     override val mainUrl = "https://voe.sx"
 }
@@ -45,66 +46,39 @@ class MetaGnathTuggers : Voe() {
     override val name = "Metagnathtuggers"
     override val mainUrl = "https://metagnathtuggers.com"
 }
+
 class FileLions : Filesim() {
     override val name = "Filelions"
-    override var mainUrl = "https://filelions.to"
+    override val mainUrl = "https://filelions.to"
 }
+
 class StreamHubGg : Streamhub() {
-    override var name = "Streamhub Gg"
-    override var mainUrl = "https://streamhub.gg"
+    override val name = "Streamhub Gg"
+    override val mainUrl = "https://streamhub.gg"
 }
+
 class StreamTapeAdblockuser : StreamTape() {
-    override var mainUrl = "https://streamtapeadblockuser.xyz"
+    override val mainUrl = "https://streamtapeadblockuser.xyz"
 }
 
 class StreamTapeTo : StreamTape() {
-    override var mainUrl = "https://streamtape.to"
+    override val mainUrl = "https://streamtape.to"
 }
 
-class Mixdrp : MixDrop(){
-    override var mainUrl = "https://mixdrp.to"
+class Mixdrp : MixDrop() {
+    override val mainUrl = "https://mixdrp.to"
 }
 
 class DoodReExtractor : DoodLaExtractor() {
-    override var mainUrl = "https://dood.re"
+    override val mainUrl = "https://dood.re"
 }
 
-class Streamzz : Streamcrypt() {
-    override var name = "Streamzz"
-    override var mainUrl = "https://streamzz.to"
-}
-open class Doodstream : ExtractorApi() {
-    override val name = "Doodstream"
-    override val mainUrl = "https://doodstream.com"
-    override val requiresReferer = false
-override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        val response = app.get(url, referer = referer)
-        val script = if (!getPacked(response.text).isNullOrEmpty()) {
-            getAndUnpack(response.text)
-        } else {
-            response.document.selectFirst("script:containsData(sources:)")?.data()
-        }
-        val m3u8 =
-            Regex("file:\\s*\"(.*?m3u8.*?)\"").find(script ?: return)?.groupValues?.getOrNull(1)
-        callback.invoke(
-            ExtractorLink(
-                name,
-                name,
-                m3u8 ?: return,
-                "$mainUrl/",
-                Qualities.Unknown.value,
-                INFER_TYPE
-            )
-        )
-    }
-
+class Streamzz : StreamTape() {
+    override val name = "Streamzz"
+    override val mainUrl = "https://streamzz.to"
 }
 
+// Doodstream-Extractor-Klasse
 open class Doodstream : ExtractorApi() {
     override val name = "Doodstream"
     override val mainUrl = "https://doodstream.com"
@@ -125,6 +99,7 @@ open class Doodstream : ExtractorApi() {
         val quality = Regex("\\d{3,4}p").find(
             response0.substringAfter("<title>").substringBefore("</title>")
         )?.groupValues?.get(0)
+
         callback.invoke(
             ExtractorLink(
                 this.name,
@@ -142,29 +117,37 @@ open class Doodstream : ExtractorApi() {
             "${it.scheme}://${it.host}"
         }
     }
-
 }
+
+// Moflix Extractor API - Beispielimplementierung
+open class MoflixExtractor : ExtractorApi() {
+    override val name = "MoflixExtractor"
+    override val mainUrl = "https://moflix-extractor.com"
+    override val requiresReferer = true
+
     override suspend fun getUrl(
         url: String,
         referer: String?,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val req = app.get(url)
-        val host = getBaseUrl(req.url)
-        val response0 = req.text
-        val md5 = host + (Regex("/pass_md5/[^']*").find(response0)?.value ?: return)
-        val trueUrl =
-            app.get(md5, referer = req.url).text + "qWMG3yc6F5?token=" + md5.substringAfterLast("/")
-        val quality = Regex("\\d{3,4}p").find(
-            response0.substringAfter("<titl     ExtractorLink(
-                        this.name,
-                        this.name,
-                        link,
-                        "",
-                        Qualities.Unknown.value
-                    )
-                )
-            }
+        val req = app.get(url, referer = referer)
+        val script = if (!getPacked(req.text).isNullOrEmpty()) {
+            getAndUnpack(req.text)
+        } else {
+            req.document.selectFirst("script:containsData(sources:)")?.data()
+        }
+        val m3u8 = Regex("file:\\s*\"(.*?m3u8.*?)\"").find(script ?: return)?.groupValues?.getOrNull(1)
+
+        callback.invoke(
+            ExtractorLink(
+                name,
+                name,
+                m3u8 ?: return,
+                "$mainUrl/",
+                Qualities.Unknown.value,
+                false
+            )
+        )
     }
 }
