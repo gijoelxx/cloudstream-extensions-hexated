441d5bf1cbaea6cef1a8f08b675de5aabd3036ea
gijoelxx <technoticx@gmail.com>
2024-11-03 02:49:31 +0100

    Update Movie2k.kt

--- Xcineio/src/main/kotlin/com/hexated/Movie2k.kt
+++ Xcineio/src/main/kotlin/com/hexated/Movie2k.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
 
 open class Movie2k : MainAPI() {
-    override var name = "XCine"
+    override var name = "Movie2k"
     override var mainUrl = "https://www2.movie2k.ch"
     override var lang = "de"
     override val hasQuickSearch = true
