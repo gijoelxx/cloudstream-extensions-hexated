37cca67e010ab669de146324b13faec8fb6bb12c
gijoelxx <technoticx@gmail.com>
2024-11-05 22:57:10 +0100

    Update Movie2kPlugin.kt

--- Movie2k/src/main/kotlin/com/hexated/Movie2kPlugin.kt
+++ Movie2k/src/main/kotlin/com/hexated/Movie2kPlugin.kt
@@ -7,6 +7,18 @@ import android.content.Context
 class Movie2kPlugin : Plugin() {
     override fun load(context: Context) {
       registerMainAPI(Movie2k))
+      registerExtractorAPI(Dooood())
+        registerExtractorAPI(StreamHubGg())
+        registerExtractorAPI(VoeSx())
+        registerExtractorAPI(MetaGnathTuggers())
+        registerExtractorAPI(FileLions())
+        registerExtractorAPI(StreamTapeTo())
+        registerExtractorAPI(Mixdrp())
+        registerExtractorAPI(DoodReExtractor())
+        registerExtractorAPI(Streamzz())
+        registerExtractorAPI(Streamcrypt())
+    }
+}
 
 
      }
